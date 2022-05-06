7f96f09a47bc08bd61ccdc0244c11b40cc7c382f
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-05-07 05:26:11 +0800

    all comits

--- scripts_base/scripts_base/urls.py
+++ scripts_base/scripts_base/urls.py
@@ -1,4 +1,4 @@
-"""script_base URL Configuration
+"""scripts_base URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- scripts_base/scripts_base/wsgi.py
+++ scripts_base/scripts_base/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for script_base project.
+WSGI config for scripts_base project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'script_base.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scripts_base.settings')
 
 application = get_wsgi_application()
