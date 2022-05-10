87bb0866971b10007ce0fe4659f492292c48b3df
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-05-10 19:53:07 +0800

    add url to urls_product.py delete_product

--- scripts_base/app_scripts/urls_product.py
+++ scripts_base/app_scripts/urls_product.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from app_scripts.views import SecondaryMarketView, AddProductView, GetAllProductsView
+from app_scripts.views import (
+    SecondaryMarketView, AddProductView, GetAllProductsView, DeleteProductView)
 
 
 urlpatterns = [
     path('add_product', AddProductView.as_view(), name='add_product'),
     path('get_all_products', GetAllProductsView.as_view(), name='get_all_products'),
+    path('delete_product', DeleteProductView.as_view(), name='delete_product'),
     path('secondary', SecondaryMarketView.as_view(), name='secondary_market'),
 ]
 
