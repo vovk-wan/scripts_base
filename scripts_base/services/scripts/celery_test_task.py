3f699f425bc14e3e4f61e44e9e04588829ce2a2a
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-05-18 22:52:19 +0800

    Edit celery_test_task.py

--- scripts_base/services/scripts/celery_test_task.py
+++ scripts_base/services/scripts/celery_test_task.py
@@ -5,5 +5,5 @@ from scripts_base.celery import app
 
 @app.task
 def my_task(value_str: str, value_int: int) -> dict:
-    x = 10**10**value_int
+    x = 10**value_int**5
     return {'str': value_str, 'int': x, 'time': datetime.datetime.now()}
