2459ea83ee285dea998eb1fd6a589231885df5f5
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-06-01 18:52:29 +0800

    edit script_base celery.py

--- scripts_base/scripts_base/celery.py
+++ scripts_base/scripts_base/celery.py
@@ -5,7 +5,10 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scripts_base.settings')
 
-app = Celery('scripts_base')
+app = Celery('scripts_base') # TODO old config
+
+# Using a string here means the worker doesn't have to serialize
+# app = Celery('scripts_base', backend='redis', broker='redis://localhost:6379')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -14,9 +17,14 @@ app = Celery('scripts_base')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
-app.autodiscover_tasks()
+app.autodiscover_tasks()  # TODO old config
+
+# app.autodiscover_tasks()@app.task(bind=True)
 
 
-@app.task(bind=True)
+@app.task(bind=True)   # TODO old config
 def debug_task(self):
     print(f'Request: {self.request!r}')
+    print('Request: {0!r}'.format(self.request))
+
+
